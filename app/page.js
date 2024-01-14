c71f92511d36648641bc7c7a3101ba24ab0696d7
lcohen730 <lcohen730@gmail.com>
2024-01-14 16:31:09 -0500

    Add Like Button Client Component and Adjust Page File Accordingly

--- app/page.js
+++ app/page.js
@@ -1,17 +1,11 @@
-import { useState } from 'react';
+import LikeButton from "./like-button";
 
 function Header({ title }) {
     return <h1>{title ? title : "Default title"}</h1>
 }
 
-function HomePage() {
-    const names = ["Ada Lovelace", "Grace Hopper", "Margaret Hamilton"]
-
-    const [likes, setLikes] = React.useState(0)
-
-    function handleClick() {
-        setLikes(likes + 1)
-    }
+export default function HomePage() {
+    const names = ["Ada Lovelace", "Grace Hopper", "Margaret Hamilton"];
 
     return (
         <div>
@@ -21,7 +15,7 @@ function HomePage() {
                     <li key={name}>{name}</li>
                 ))}
             </ul>
-            <button onClick={handleClick}>Like ({likes})</button>
+            <LikeButton />
         </div>
-    )
+    );
 }
